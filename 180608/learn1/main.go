d7ef6207a48bdc11f4a6c9c6477c8e1d948ce00a
CRORCR <xingzhiheyi_code@163.com>
2018-06-08 18:36:17 +0800

    Initial commit

---
+++ 180608/learn1/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now().Minute()
+
+	_ := numJewelsInStones("aAasfcasf", "aAAbbb")
+	end := time.Now().Minute()
+	fmt.Println(end-start)
+}
+
+//我考虑的是strings包下有一个count计数的函数,可以拿到J的每个字符去统计
+func numJewelsInStones(J string, S string) int {
+	var count int
+	for i:=0;i<len(J);i++{
+		u := string(J[i])
+		count += strings.Count(S, u)
+	}
+	return count
+}
+
+//大神代码,使用 2个range,第一个字符==第二个字符就++
+//快在使用了range
+func numJewelsInStones2(J string, S string) int {
+	a:=0
+	for _,v:=range J{
+		for _,v1:=range S{
+			if v==v1{
+				a++
+			}
+		}
+	}
+	return a
+}
